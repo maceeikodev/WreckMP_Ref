e1394594e5c2485eaa406dce592100b2f6b71b2e
Maceeiko <osadnik.osadnikovy@gmail.com>
2025-06-01 19:18:40 +0200

    Synced properties

---
+++ BasicSyncedProperties.cs
@@ -0,0 +1,321 @@
+﻿using UnityEngine;
+
+namespace WreckMP_Ref
+{
+    public class SyncedFloat : SyncedProperty<float>
+    {
+        public SyncedFloat(string uid, bool registerTimer = false) : base(uid, registerTimer) { }
+
+        /// <summary>
+        /// When new value is set, if the difference is bigger than this value the event is sent.
+        /// </summary>
+        public float tolerance = 0f;
+        float lastUpdatedValue;
+
+        // Last updated value is technically oldVal, but when it was last sent
+        // So if value is gradually increased by small steps, it will be updated when the change is big enough
+        protected override bool IsChanged(float oldVal, float newVal)
+        {
+            float diff = lastUpdatedValue - newVal;
+            if (diff < 0) diff = -diff;
+            return diff > tolerance;
+        }
+
+        protected override void Write(GameEventWriter p, bool initial)
+        {
+            p.Write(_value);
+            lastUpdatedValue = _value;
+        }
+
+        protected override void Read(GameEventReader p)
+        {
+            _value = p.ReadSingle();
+        }
+    }
+
+    public class SyncedDouble : SyncedProperty<double>
+    {
+        public SyncedDouble(string uid, bool registerTimer = false) : base(uid, registerTimer) { }
+
+        /// <summary>
+        /// When new value is set, if the difference is bigger than this value the event is sent.
+        /// </summary>
+        public double tolerance = 0;
+        double lastUpdatedValue;
+
+        // Last updated value is technically oldVal, but when it was last sent
+        // So if value is gradually increased by small steps, it will be updated when the change is big enough
+
+        protected override bool IsChanged(double oldVal, double newVal)
+        {
+            double diff = lastUpdatedValue - newVal;
+            if (diff < 0) diff = -diff;
+            return diff > tolerance;
+        }
+
+        protected override void Write(GameEventWriter p, bool initial)
+        {
+            p.Write(_value);
+            lastUpdatedValue = _value;
+        }
+
+        protected override void Read(GameEventReader p)
+        {
+            _value = p.ReadDouble();
+        }
+    }
+
+    public class SyncedInt16 : SyncedProperty<short>
+    {
+        public SyncedInt16(string uid, bool registerTimer = false) : base(uid, registerTimer) { }
+
+        /// <summary>
+        /// When new value is set, if the difference is bigger than this value the event is sent.
+        /// </summary>
+        public short tolerance = 0;
+        short lastUpdatedValue;
+
+        // Last updated value is technically oldVal, but when it was last sent
+        // So if value is gradually increased by small steps, it will be updated when the change is big enough
+
+        protected override bool IsChanged(short oldVal, short newVal)
+        {
+            int diff = lastUpdatedValue - newVal;
+            if (diff < 0) diff = -diff;
+            return diff > tolerance;
+        }
+
+        protected override void Write(GameEventWriter p, bool initial)
+        {
+            p.Write(_value);
+            lastUpdatedValue = _value;
+        }
+
+        protected override void Read(GameEventReader p)
+        {
+            _value = p.ReadInt16();
+        }
+    }
+
+    public class SyncedInt32 : SyncedProperty<int>
+    {
+        public SyncedInt32(string uid, bool registerTimer = false) : base(uid, registerTimer) { }
+
+        /// <summary>
+        /// When new value is set, if the difference is bigger than this value the event is sent.
+        /// </summary>
+        public int tolerance = 0;
+        int lastUpdatedValue;
+
+        // Last updated value is technically oldVal, but when it was last sent
+        // So if value is gradually increased by small steps, it will be updated when the change is big enough
+
+        protected override bool IsChanged(int oldVal, int newVal)
+        {
+            int diff = lastUpdatedValue - newVal;
+            if (diff < 0) diff = -diff;
+            return diff > tolerance;
+        }
+
+        protected override void Write(GameEventWriter p, bool initial)
+        {
+            p.Write(_value);
+            lastUpdatedValue = _value;
+        }
+
+        protected override void Read(GameEventReader p)
+        {
+            _value = p.ReadInt32();
+        }
+    }
+
+    public class SyncedInt64 : SyncedProperty<long>
+    {
+        public SyncedInt64(string uid, bool registerTimer = false) : base(uid, registerTimer) { }
+
+        /// <summary>
+        /// When new value is set, if the difference is bigger than this value the event is sent.
+        /// </summary>
+        public long tolerance = 0;
+        long lastUpdatedValue;
+
+        // Last updated value is technically oldVal, but when it was last sent
+        // So if value is gradually increased by small steps, it will be updated when the change is big enough
+
+        protected override bool IsChanged(long oldVal, long newVal)
+        {
+            long diff = lastUpdatedValue - newVal;
+            if (diff < 0) diff = -diff;
+            return diff > tolerance;
+        }
+
+        protected override void Write(GameEventWriter p, bool initial)
+        {
+            p.Write(_value);
+            lastUpdatedValue = _value;
+        }
+
+        protected override void Read(GameEventReader p)
+        {
+            _value = p.ReadInt64();
+        }
+    }
+
+    public class SyncedBoolean : SyncedProperty<bool>
+    {
+        public SyncedBoolean(string uid, bool registerTimer = false) : base(uid, registerTimer) { }
+
+        protected override bool IsChanged(bool oldVal, bool newVal)
+        {
+            return oldVal != newVal;
+        }
+
+        protected override void Write(GameEventWriter p, bool initial)
+        {
+            p.Write(_value);
+        }
+
+        protected override void Read(GameEventReader p)
+        {
+            _value = p.ReadBoolean();
+        }
+    }
+
+    public class SyncedString : SyncedProperty<string>
+    {
+        public SyncedString(string uid, bool registerTimer = false) : base(uid, registerTimer) { }
+
+        protected override bool IsChanged(string oldVal, string newVal)
+        {
+            return oldVal != newVal;
+        }
+
+        protected override void Write(GameEventWriter p, bool initial)
+        {
+            if (_value == null) return;
+            p.Write(_value);
+        }
+
+        protected override void Read(GameEventReader p)
+        {
+            if (p.UnreadLength() == 0)
+            {
+                _value = null;
+                return;
+            }
+            _value = p.ReadString();
+        }
+    }
+
+    public class SyncedVector2 : SyncedProperty<Vector2>
+    {
+        public SyncedVector2(string uid, bool registerTimer = false) : base(uid, registerTimer) { }
+
+        /// <summary>
+        /// When new value is set, if the magnitude difference is bigger than this value the event is sent.
+        /// </summary>
+        public float tolerance = 0f;
+        Vector2 lastUpdatedValue;
+
+        // Last updated value is technically oldVal, but when it was last sent
+        // So if value is gradually increased by small steps, it will be updated when the change is big enough
+
+        protected override bool IsChanged(Vector2 oldVal, Vector2 newVal)
+        {
+            var diff = lastUpdatedValue - newVal;
+            return diff.sqrMagnitude > (tolerance * tolerance);
+        }
+
+        protected override void Write(GameEventWriter p, bool initial)
+        {
+            p.Write(_value.x);
+            p.Write(_value.y);
+            lastUpdatedValue = _value;
+        }
+
+        protected override void Read(GameEventReader p)
+        {
+            float x = p.ReadSingle();
+            float y = p.ReadSingle();
+            _value = new Vector2(x, y);
+        }
+    }
+
+    public class SyncedVector3 : SyncedProperty<Vector3>
+    {
+        public SyncedVector3(string uid, bool registerTimer = false) : base(uid, registerTimer) { }
+
+        /// <summary>
+        /// When new value is set, if the magnitude difference is bigger than this value the event is sent.
+        /// </summary>
+        public float tolerance = 0f;
+        Vector3 lastUpdatedValue;
+
+        // Last updated value is technically oldVal, but when it was last sent
+        // So if value is gradually increased by small steps, it will be updated when the change is big enough
+
+        protected override bool IsChanged(Vector3 oldVal, Vector3 newVal)
+        {
+            var diff = lastUpdatedValue - newVal;
+            return diff.sqrMagnitude > (tolerance * tolerance);
+        }
+
+        protected override void Write(GameEventWriter p, bool initial)
+        {
+            p.Write(_value);
+            lastUpdatedValue = _value;
+        }
+
+        protected override void Read(GameEventReader p)
+        {
+            _value = p.ReadVector3();
+        }
+    }
+
+    public class SyncedColor : SyncedProperty<Color>
+    {
+        public SyncedColor(string uid, bool registerTimer = false) : base(uid, registerTimer) { }
+
+        /// <summary>
+        /// When new value is set, if the sum of differences of all channels is bigger than this value the event is sent.
+        /// </summary>
+        public float tolerance = 0f;
+        Color lastUpdatedValue;
+
+        // Last updated value is technically oldVal, but when it was last sent
+        // So if value is gradually increased by small steps, it will be updated when the change is big enough
+
+        protected override bool IsChanged(Color oldVal, Color newVal)
+        {
+            float dr = lastUpdatedValue.r - newVal.r;
+            if (dr < 0) dr = -dr;
+            float dg = lastUpdatedValue.g - newVal.g;
+            if (dg < 0) dg = -dg;
+            float db = lastUpdatedValue.b - newVal.b;
+            if (db < 0) db = -db;
+            float da = lastUpdatedValue.a - newVal.a;
+            if (da < 0) da = -da;
+
+            float diff = dr + dg + db + da;
+            return diff > tolerance;
+        }
+
+        protected override void Write(GameEventWriter p, bool initial)
+        {
+            p.Write(_value.r);
+            p.Write(_value.g);
+            p.Write(_value.b);
+            p.Write(_value.a);
+            lastUpdatedValue = _value;
+        }
+
+        protected override void Read(GameEventReader p)
+        {
+            float r = p.ReadSingle();
+            float g = p.ReadSingle();
+            float b = p.ReadSingle();
+            float a = p.ReadSingle();
+            _value = new Color(r, g, b, a);
+        }
+    }
+}
---
+++ SyncedProperty.cs
@@ -0,0 +1,162 @@
+﻿using MSCLoader;
+using System;
+using UnityEngine;
+
+namespace WreckMP_Ref
+{
+    /// <summary>
+    /// Base class for synced properties. This class is abstract. For syncing custom types inherit this class to implement your own synced property.
+    /// </summary>
+    public abstract class SyncedProperty<T>
+    {
+        string _uniqueName;
+        /// <summary>
+        /// The unique name of the game event used to send updates.
+        /// </summary>
+        public string UniqueName => _uniqueName;
+        /// <summary>
+        /// Whether to send the event primarily over TCP. Default is false. Set to true if you only send the update once in a while and isn't very time sensitive
+        /// </summary>
+        public bool sendOverTCP = false;
+
+        GameEvent gameEvent;
+
+        protected T _value;
+        public T Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                if (IsChanged(_value, value))
+                {
+                    if (currentCooldown > 0f)
+                    {
+                        hasChangeToSend = true;
+                    }
+                    else
+                    {
+                        SendUpdate();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Fired when received an update over the network and the value changes
+        /// </summary>
+        public event Action ReceivedUpdate;
+
+        bool registeredTimer = false, hasChangeToSend = false;
+        float _updateCooldown = 0f, currentCooldown = 0f;
+        /// <summary>
+        /// Cooldown for sending update packets in seconds. Default is 0
+        /// </summary>
+        public float UpdateCooldown
+        {
+            get { return _updateCooldown; }
+            set
+            {
+                if (!registeredTimer)
+                {
+                    throw new InvalidOperationException("Can't set cooldown because the property is not registered in the timer");
+                }
+                _updateCooldown = value;
+            }
+        }
+
+        /// <summary>
+        /// Creates an instance
+        /// </summary>
+        /// <param name="uid">The unique name of the game event used to send updates.</param>
+        /// <param name="registerTimer">Set to true if you're going to use UpdateCooldown, otherwise keep on false to improve performance. Only use UpdateCooldown if you change the value often</param>
+        protected SyncedProperty(string uid, bool registerTimer)
+        {
+            _uniqueName = uid;
+
+            if (WreckMPGlobals.IsMultiplayerSession)
+            {
+                gameEvent = new GameEvent(uid, OnGameEvent);
+                if (WreckMPGlobals.IsHost)
+                {
+                    WreckMPGlobals.OnMemberReady(OnMemberReady);
+                }
+
+                if (registerTimer) mp_registerTimer();
+            }
+        }
+
+        void mp_registerTimer()
+        {
+            registeredTimer = WreckMP.NetTimer.RegisterTimer(TimerUpdate);
+            if (!registeredTimer)
+                ModConsole.LogError($"Failed to register synced property '{_uniqueName}' because timer is not initialized");
+        }
+
+        void OnGameEvent(GameEventReader p)
+        {
+            Read(p);
+            ReceivedUpdate?.Invoke();
+        }
+
+        void OnMemberReady(ulong target)
+        {
+            SendUpdate(target, true, true);
+        }
+
+        /// <summary>
+        /// Send update event
+        /// </summary>
+        /// <param name="target">Target user. 0 sends to everyone</param>
+        /// <param name="safe">If null, sendOverTCP field is used</param>
+        /// <param name="initial">True if initial sync. This argument is only passed to the Write method</param>
+        public void SendUpdate(ulong target = 0, bool? safe = null, bool initial = false)
+        {
+            using (var p = gameEvent.Writer())
+            {
+                Write(p, initial);
+                p.Send(target, safe ?? sendOverTCP);
+                currentCooldown = _updateCooldown;
+            }
+        }
+
+        /// <summary>
+        /// This function is called when the property Value is set to see whether the change should be sent to others.
+        /// </summary>
+        /// <param name="oldVal">Old value</param>
+        /// <param name="newVal">New value</param>
+        /// <returns>If true, the event is sent.</returns>
+        protected abstract bool IsChanged(T oldVal, T newVal);
+
+        /// <summary>
+        /// Serialize the update data
+        /// </summary>
+        /// <param name="p"></param>
+        protected abstract void Write(GameEventWriter p, bool initial);
+
+        /// <summary>
+        /// Deserialize the update data. WARNING!!! When writing to Value, write to _value instead to prevent another event being sent!
+        /// </summary>
+        /// <param name="p"></param>
+        protected abstract void Read(GameEventReader p);
+
+        /// <summary>
+        /// DO NOT USE! This method is called internally to calculate cooldown timers.
+        /// </summary>
+        public void TimerUpdate()
+        {
+            if (currentCooldown > 0f)
+            {
+                currentCooldown -= Time.deltaTime;
+            }
+            else if (hasChangeToSend)
+            {
+                hasChangeToSend = false;
+                SendUpdate();
+            }
+        }
+    }
+}
