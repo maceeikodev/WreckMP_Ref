11725fb1e36da8cb72c51e8d8e415ea7d1032044
Maceeiko <osadnik.osadnikovy@gmail.com>
2025-06-01 16:04:19 +0200

    OwnedRigidbody

---
+++ NetRigidbodyManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+
+namespace WreckMP_Ref
+{
+    public static class NetRigidbodyManager
+    {
+        /// <summary>
+        /// Register a rigidbody. Do NOT register rigidbodies created in PreLoad / OnLoad / PostLoad! They are registered automatically.
+        /// </summary>
+        /// <param name="rb">The rigidbody to register.</param>
+        /// <param name="hash">Unique hash to identify the rigibody.</param>
+        /// <returns>The newly created and registered OwnedRigidbody object.</returns>
+        public static OwnedRigidbody RegisterRigidbody(this Rigidbody rb, int hash)
+        {
+            
+        }
+
+        /// <summary>
+        /// Get OwnedRigidbody by its hash.
+        /// </summary>
+        /// <param name="hash"> The hash of the requested rigidbody.</param>
+        /// <returns>The requested OwnedRigidbody, if found, otherwise null.</returns>
+        public static OwnedRigidbody GetOwnedRigidbody(int hash)
+        {
+
+        }
+
+        /// <summary>
+        /// Get a hash of a rigidbody.
+        /// </summary>
+        /// <param name="rb">The rigidbody to look for.</param>
+        /// <returns>The hash of the requested rigidbody, if found, otherwise 0.</returns>
+        public static int GetRigidbodyHash(this Rigidbody rb)
+        {
+            if (!WreckMPGlobals.IsMultiplayerSession) return 0;
+            return mp_GetRigidbodyHash(rb);
+        }
+
+        /// <summary>
+        /// Set a rigidbody's owner to local player.
+        /// </summary>
+        /// <param name="rigidbody">The rigidbody to change owner of.</param>
+        public static void RequestOwnership(this Rigidbody rigidbody)
+        {
+            if (!WreckMPGlobals.IsMultiplayerSession) return;
+            mp_RequestOwnership(rigidbody);
+        }
+
+        /// <summary>
+        /// Set a rigidbody's owner to local player.
+        /// </summary>
+        /// <param name="rigidbody">The rigidbody to change owner of.</param>
+        public static void RequestOwnership(this OwnedRigidbody rigidbody)
+        {
+            if (!WreckMPGlobals.IsMultiplayerSession) return;
+            finish
+        }
+
+        static OwnedRigidbody mp_RegisterRigidbody(Rigidbody rb, int hash)
+        {
+
+        }
+
+        static OwnedRigidbody mp_GetOwnedRigidbody(int hash)
+        {
+
+        }
+
+        static int mp_GetRigidbodyHash(Rigidbody rb)
+        {
+            return WreckMP.NetRigidbodyManager.GetRigidbodyHash(rb);
+        }
+
+        static void mp_RequestOwnership(Rigidbody rigidbody)
+        {
+            WreckMP.NetRigidbodyManager.RequestOwnership(rigidbody);
+        }
+    }
+}
---
+++ OwnedRigidbody.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+
+namespace WreckMP_Ref
+{
+    /// <summary>
+    /// Helper class for a rigidbody with an owner.
+    /// </summary>
+    public class OwnedRigidbody
+    {
+        Rigidbody singleplayer_rigidbody;
+        object mp_ref;
+
+        /// <summary>
+        /// The transform of the rigidbody.
+        /// </summary>
+        public Transform OwnerTransform
+        {
+            get
+            {
+                if (!WreckMPGlobals.IsMultiplayerSession)
+                {
+                    return singleplayer_rigidbody == null ? null : singleplayer_rigidbody.transform;
+                }
+                return mp_ownertf();
+            }
+        }
+
+        /// <summary>
+        /// The Steam ID of the rigidbody owner. The owner is overriding the rigidbody's properties for other players when they update.
+        /// </summary>
+        public ulong OwnerID
+        {
+            get
+            {
+                if (!WreckMPGlobals.IsMultiplayerSession) return 0;
+                return mp_owner();
+            }
+        }
+
+        /// <summary>
+        /// The rigidbody object. Can be null, as this may be sometimes destroyed and later recreated again. OwnerTransform keeps reference to the object even if the rigidbody is destroyed.
+        /// </summary>
+        public Rigidbody Rigidbody
+        {
+            get
+            {
+                if (!WreckMPGlobals.IsMultiplayerSession) return singleplayer_rigidbody;
+                return mp_rb();
+            }
+        }
+
+        internal OwnedRigidbody(object mp_orb)
+        {
+            mp_ref = mp_orb;
+        }
+
+        internal OwnedRigidbody(Rigidbody sp_rigidbody)
+        {
+            singleplayer_rigidbody = sp_rigidbody;
+        }
+
+        Transform mp_ownertf()
+        {
+            var mp_orb = mp_ref as WreckMP.OwnedRigidbody;
+            return mp_orb.transform;
+        }
+
+        ulong mp_owner()
+        {
+            var mp_orb = mp_ref as WreckMP.OwnedRigidbody;
+            return mp_orb.OwnerID;
+        }
+
+        Rigidbody mp_rb()
+        {
+            var mp_orb = mp_ref as WreckMP.OwnedRigidbody;
+            return mp_orb.Rigidbody;
+        }
+    }
+}
